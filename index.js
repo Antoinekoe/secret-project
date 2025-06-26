ab78cc9be067b5dee4917e83ec2ecf84829f68c1
Antoinekoe <antoine.koe@gmail.com>
2025-06-26 22:20:18 +0200

    Delete bodyparser as it's not needed

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 let lastName = ["Permann", "Ziner", "Kade", "Nutt"];
 let firstName = ["Sue", "Dee", "Barry", "Hazel"];
 
